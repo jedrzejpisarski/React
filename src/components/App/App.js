ad55e6ae3e09db6acbc91e931f1b4267416a641e
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-08-19 17:56:42 +0200

    change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import List from '../List.js';
 
 class App extends React.Component {
   render() {
@@ -6,6 +7,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello world!</h2>
+        <List />
       </main>
     )
   }
