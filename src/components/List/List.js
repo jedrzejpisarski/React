880f16ad4bbd53a7af5131658e2bc89b9931e3df
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-11-15 16:02:43 +0100

    change

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,15 +3,30 @@ import Hero from '../Hero/Hero.js'
 import Column from '../Column/Column.js'
 import styles from './List.scss'
 import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
+
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    description: PropTypes.node,
+    columns: PropTypes.array,
+  }
+
+  static defaultProps = {
+    description: settings.defaultListDescription,
+  }
+
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} image={this.props.image} />
-        <div className={styles.columns}>
+          <div className={styles.description}>
+            {this.props.description}
+          </div>
+          <div className={styles.columns}>
             {this.props.columns.map(col => <Column key={col.title} icon={col.icon} title={col.title} />)}
-        </div>
+          </div>
       </section>
     )
   }
@@ -21,7 +36,7 @@ List.propTypes = {
     columns: PropTypes.array.isRequired,
     title: PropTypes.string,
     image: PropTypes.string,
-    description: PropTypes.string
+    description: PropTypes.string,
 }
 
 export default List;
