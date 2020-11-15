d1108e764354707831acdf7484c33ab68218bcb6
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-11-15 17:28:49 +0100

    change

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import Hero from '../Hero/Hero.js'
-import Column from '../Column/Column.js'
-import styles from './List.scss'
+import Hero from '../Hero/Hero.js';
+import Column from '../Column/Column.js';
+import styles from './List.scss';
 import PropTypes from 'prop-types';
 import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
   state = {
@@ -28,6 +29,9 @@ class List extends React.Component {
             {this.state.columns.map(({key, ...columnProps}) => (
               <Column key={key} {...columnProps} />
             ))}
+          </div> 
+          <div className={styles.creator}>
+              <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
           </div>  
       </section>
     )
