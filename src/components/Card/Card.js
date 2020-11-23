efb4f5a3b7091605583ba74b1cbffd4fabdaf85d
Kamil <jaworskiebiznes@gmail.com>
2020-11-23 22:57:35 +0000

    add cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,10 @@
-import {settings, listData} from '../../data/dataStore';
-import styles from './Card.scss';
+import React from "react";
+import styles from "./Card.scss";
 
-const Card = ({title}) => {
-    return (
-        <div className={styles.component}>
-            <h2>{ title }</h2>
-        </div>
-    )
+export const Card = (props) => {
+  return (
+    <div className={styles.component}>
+      <h2>{props.title}</h2>
+    </div>
+  );
 };
-
-export default Card; 
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import styles from './Column.scss';
-import PropTypes from 'prop-types';
-import Icon from '../Icon/Icon';
-import Card from '../Card/Card';
+import React, { useState } from "react";
+import styles from "./Column.scss";
+import PropTypes from "prop-types";
+import Icon from "../Icon/Icon";
+import { Card } from "../Card/Card";
+import Creator from "../Creator/Creator.js";
 
-const Column = props => {
-
-    state = {
-        cards: this.props.cards || [],
-      }
-
-    return (
-        <div className={styles.component}>
-            <h2>
-                <Icon name={props.icon} />
-                { props.title }</h2>
-        </div>
-    )
+const Column = (props) => {
+  const [cards, setCards] = useState(props.cards);
+  const addCard = (arg) => {
+    setCards([
+      ...cards,
+      { key: cards.length ? cards[cards.length - 1].key + 1 : 0, title: arg },
+    ]);
+    console.log(arg);
+  };
+  return (
+    <div className={styles.component}>
+      <h2>
+        <Icon name={props.icon} />
+        {props.title}
+      </h2>
+      {cards.map((card) => {
+        return <Card key={card.key} title={card.title} />;
+      })}
+      <div className={styles.creator}>
+        <Creator text={"add card"} action={(title) => addCard(title)} />
+      </div>
+    </div>
+  );
 };
 
 Column.propTypes = {
   title: PropTypes.string,
-  icon: PropTypes.string
-}
+  icon: PropTypes.string,
+};
 
 export default Column;
