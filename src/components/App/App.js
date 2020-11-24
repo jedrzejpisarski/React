89b1795b083e2e06aaa37ac6f50c20cdc21e778f
Jędrzej Pisarski <jedrzej.pisarski@wp.pl>
2020-11-24 21:45:58 +0100

    redux aded

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import styles from '../App/App.scss';
-import List from '../List/List.js';
-import { pageContents, listData } from '../../data/dataStore';
+//import List from '../List/List.js';
 
 class App extends React.Component {
+   
+    static propTypes = {
+        title: PropTypes.node,
+        subtitle: PropTypes.node,
+      }
+      
     render() {
+        const {title, subtitle} = this.props;
         return (
-            <main className={styles.component}>
-                <h1 className={styles.title}>{pageContents.title}</h1>
-                <h2 className={styles.subtitle}>{pageContents.subtitle}</h2>
-                <List {...listData} />
-            </main>
+          <main className={styles.component}>
+            <h1 className={styles.title}>{title}</h1>
+            <h2 className={styles.subtitle}>{subtitle}</h2>
+            {/*
+            <List {...listData} />
+            */}
+          </main>
         );
-    }
+      }
 }
 
 export default App;
