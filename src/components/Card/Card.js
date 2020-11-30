2eba536231d328f54d0648aaf2f34c35e4343bde
Kamil <jaworskiebiznes@gmail.com>
2020-11-30 14:25:45 +0000

    pull master

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import styles from './Card.scss';
+import React from "react";
+import styles from "./Card.scss";
+import PropTypes from "prop-types";
 
 export const Card = (props) => {
-    return (
-        <div className={styles.component}>
-            <h2>{props.title}</h2>
-        </div>
-    );
+  return (
+    <div className={styles.component}>
+      <h2>{props.title}</h2>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string,
 };
